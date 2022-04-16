bbbb5bd2e958caa3f4ffa6555e9bbea8fb7d07fa
gpain1999 <80475152+gpain1999@users.noreply.github.com>
2022-04-16 18:51:48 +0200

    ouististi

--- question_2.R
+++ question_2.R
@@ -13,3 +13,15 @@ print(load("AdvRegr_4_nels.RData"))
 #repartition %
 round(prop.table(with(nels, table(region, useNA = "ifany")))*100,2)
 #South is bigger
+
+### Contingency table
+(tab2 <- with(nels, table(ses, fa.educ,region)))
+(ptab2 <- round(prop.table(tab2, margin = 2) * 100, 1))
+
+#dataset
+
+qq2 <- as.data.frame(tab2, responseName = "N")
+
+### Saturated model
+fit2 <- glm(N ~ (ses + fa.educ + region)^3, family = poisson, data = qq2)
+summary(fit2)
