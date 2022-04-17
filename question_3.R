dd1cd18e1f05f2a707cb138443d60189853f059b
gpain1999 <80475152+gpain1999@users.noreply.github.com>
2022-04-17 10:47:19 +0200

    ouististi

--- question_3.R
+++ question_3.R
@@ -6,3 +6,20 @@ library("colorspace")
 library("xtable")
 #
 print(load("AdvRegr_4_nels.RData"))
+
+#socio-economic status of the family with the achieved level of education of the father.
+#+region + fa.wrk
+
+### Contingency table
+(tab3 <- with(nels, table(ses, fa.educ,region,fa.wrk)))
+(ptab3 <- round(prop.table(tab3, margin = 2) * 100, 1))
+
+(qq3 <- as.data.frame(tab3, responseName = "N"))
+#Problem: cross-over with no observations.
+
+#satured model
+fitp3 <- glm(N ~ (ses+fa.educ+region+fa.wrk)^4, family = poisson, data = qq3)
+fitp3.1 <- glm(N ~ (ses:fa.educ:region:fa.wrk), family = poisson, data = qq3)
+anova(fitp3,fitp3.1,test = "LRT")
+#modele fitp3.1 seems beter.
+
